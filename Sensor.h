ffd71ef7b36b1e195b019e2ddd7d92bab0b6b815
Fatima-Zahra KERDAD <fatima-zahra.kerdad@imt-atlantique.net>
2022-03-07 01:14:41 +0100

    add sensor class

---
+++ Sensor.h
@@ -0,0 +1,24 @@
+#ifndef _SENSOR_H_
+#define _SENSOR_H_
+
+#include <iostream>
+using namespace std;
+
+class Sensor{
+	protected:
+		double field;
+		double distance;
+		double detectionCapacity;
+
+	public:
+
+		Sensor(double f, double d, double c);
+		~Sensor();
+		double getField();
+		double getDistance();
+		double getDetectionCapacity();
+		virtual Sensor *clone()const=0;
+
+};
+
+#endif
